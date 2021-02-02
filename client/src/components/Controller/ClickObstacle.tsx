d209d4e34e87140b98cb0708085cb83147494b39
Magdalena Maislinger <magdalena.maislinger.mm@gmail.com>
2021-02-02 10:56:31 +0100

    + add screen setup

--- client/src/components/Controller/ClickObstacle.tsx
+++ client/src/components/Controller/ClickObstacle.tsx
@@ -1,20 +1,19 @@
 import * as React from 'react'
-import { IMovePlayer } from './Character'
 import { ObstacleButton } from './ClickObstacle.sc'
 
 interface IClickObstacle {
     setObstacle: (value: boolean) => void
     setObstacleRemoved: (value: boolean) => void
-    movePlayer: (val: IMovePlayer) => void
+    sendMessage: () => void
 }
 
-const ClickObstacle: React.FunctionComponent<IClickObstacle> = ({ setObstacle, setObstacleRemoved, movePlayer }) => {
+const ClickObstacle: React.FunctionComponent<IClickObstacle> = ({ setObstacle, setObstacleRemoved, sendMessage }) => {
     return (
         <ObstacleButton
             onClick={() => {
                 setObstacle(false)
                 setObstacleRemoved(true)
-                movePlayer({ setObstacle, obstacleRemoved: true, obstacle: false })
+                sendMessage()
                 // eslint-disable-next-line no-console
                 console.log('OBSTACLE REMOVED')
             }}
