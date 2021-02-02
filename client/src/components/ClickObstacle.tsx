6d2822582f034088bdcef1f4442db6f439bf8707
Magdalena Maislinger <magdalena.maislinger.mm@gmail.com>
2021-02-02 10:34:35 +0100

    ~Test pre commit hooks

--- client/src/components/ClickObstacle.tsx
+++ client/src/components/ClickObstacle.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import { IMovePlayer } from './Character'
 import { ObstacleButton } from './ClickObstacle.sc'
-import * as test from 'test'
 
 interface IClickObstacle {
     setObstacle: (value: boolean) => void
