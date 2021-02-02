1d733edf0e54d7faa43447c97633473976d8ffff
Robin Fellinger <rfellinger.mmt-m2020@fh-salzburg.ac.at>
2021-02-02 14:12:22 +0100

    + connection params

---
+++ server/src/services/roomService.ts
@@ -0,0 +1,23 @@
+import Room from "../classes/room";
+
+class RoomService {
+    private rooms: Array<Room>;
+
+
+    constructor() {
+        this.rooms = [];
+    }
+
+    public createRoom() {
+        let room = new Room();
+        this.rooms.push(room);
+        return room;
+    }
+
+
+    public getRoomById(roomId: string) {
+        console.log(roomId)
+        return this.rooms.filter(function (n) { return n.id === roomId })[0]
+    }
+}
+export default RoomService
