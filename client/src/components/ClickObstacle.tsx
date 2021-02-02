e9c2e306d9119da83521e5cae3d7b1f2652e675a
Magdalena Maislinger <magdalena.maislinger.mm@gmail.com>
2021-02-02 10:25:58 +0100

    ~ change obstacle style

--- client/src/components/ClickObstacle.tsx
+++ client/src/components/ClickObstacle.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { IMovePlayer } from './Character'
 import { ObstacleButton } from './ClickObstacle.sc'
+import Test from 'test'
 
 interface IClickObstacle {
     setObstacle: (value: boolean) => void
