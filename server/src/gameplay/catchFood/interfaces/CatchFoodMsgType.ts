e0eaeb590f7a4aa8fbe86926d298ff00a50884f7
Robin Fellinger <rfellinger.mmt-m2020@fh-salzburg.ac.at>
2021-02-03 17:07:48 +0100

    ~ fix obstacle event

--- server/src/gameplay/catchFood/interfaces/CatchFoodMsgType.ts
+++ server/src/gameplay/catchFood/interfaces/CatchFoodMsgType.ts
@@ -1,6 +1,7 @@
 export enum CatchFoodMsgType {
     START = 'game1/start',
+    HAS_STARTED = 'game1/hasStarted',
     MOVE = 'game1/runForward',
     OBSTACLE = 'game1/obstacle',
-    GAME_STATE = 'game1/gameState'
+    GAME_STATE = 'game1/gameState',
 }
