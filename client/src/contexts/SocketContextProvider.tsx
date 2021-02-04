777caa35850eded333e2918b1f2a4bf6727c46e5
Magdalena Maislinger <mmaislinger.mmt-m2020@fh-salzburg.ac.at>
2021-02-04 16:31:47 +0100

    Add admin controller

--- client/src/contexts/SocketContextProvider.tsx
+++ client/src/contexts/SocketContextProvider.tsx
@@ -30,6 +30,7 @@ interface IUserInitMessage {
     type?: string
     userId?: 'userInit'
     roomId?: string
+    isAdmin?: boolean
 }
 
 interface IGameFinished {
@@ -40,7 +41,7 @@ interface IGameFinished {
 const SocketContextProvider: React.FunctionComponent = ({ children }) => {
     const [screenSocket, setScreenSocket] = React.useState<Socket | undefined>(undefined)
     const [controllerSocket, setControllerSocket] = React.useState<Socket | undefined>(undefined)
-    const { setObstacle, setPlayerFinished, setPlayerRank } = React.useContext(PlayerContext)
+    const { setObstacle, setPlayerFinished, setPlayerRank, setIsPlayerAdmin } = React.useContext(PlayerContext)
 
     React.useEffect(() => {
         if (!isMobile) {
@@ -64,13 +65,14 @@ const SocketContextProvider: React.FunctionComponent = ({ children }) => {
 
     controllerSocket?.on('message', (data: IUserInitMessage | IObstacleMessage | IGameFinished) => {
         let messageData
+
         switch (data.type) {
             case 'userInit':
                 messageData = data as IUserInitMessage
                 sessionStorage.setItem('userId', messageData.userId || '')
                 sessionStorage.setItem('name', messageData.name || '')
                 sessionStorage.setItem('roomId', messageData.roomId || '')
-
+                setIsPlayerAdmin(messageData.isAdmin || false)
                 break
             case 'game1/obstacle':
                 messageData = data as IObstacleMessage
@@ -78,7 +80,6 @@ const SocketContextProvider: React.FunctionComponent = ({ children }) => {
                 break
             case 'game1/playerFinished':
                 messageData = data as IGameFinished
-
                 setPlayerFinished(true)
                 setPlayerRank(messageData.rank)
                 break
