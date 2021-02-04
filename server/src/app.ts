84f31e1c4d8fdec7b43d2cb1f849076786f61552
Robin Fellinger <rfellinger.mmt-m2020@fh-salzburg.ac.at>
2021-02-04 09:41:38 +0100

    ~ server: allow cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -32,7 +32,7 @@ const expresServer = server.app.listen({ port: PORT }, () =>
 
 const io = require("socket.io")(expresServer, {
   cors: {
-    origin: ["http://127.0.0.1:5500"],
+    origin: '*',
     methods: ["GET", "POST"],
   },
 });
