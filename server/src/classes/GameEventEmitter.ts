c0b46b1b110ace7346fe8fedccbbd95f2eda2a89
Natasha Troth <ntroth.mmt-m2020@fh-salzburg.ac.at>
2021-03-22 11:55:59 +0100

    Linting

--- server/src/classes/GameEventEmitter.ts
+++ server/src/classes/GameEventEmitter.ts
@@ -15,7 +15,7 @@
 // });
 // myEmitter.emit('event', 'a', 'b');
 
-import EventEmitter from "events"
+import EventEmitter from "events";
 
 export default class GameEventEmitter extends EventEmitter {
   private static EventEmitterInstance: GameEventEmitter = new GameEventEmitter();
@@ -23,7 +23,7 @@ export default class GameEventEmitter extends EventEmitter {
     super();
   }
 
-  public static getInstance() : GameEventEmitter{
+  public static getInstance(): GameEventEmitter {
     return this.EventEmitterInstance;
   }
 }
