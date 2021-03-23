0d80d231eb6209265ae29daabb8a0d0b18cd99e5
Natasha Troth <ntroth.mmt-m2020@fh-salzburg.ac.at>
2021-03-23 11:15:38 +0100

    Fix gameFinishedEvent test uses correct event type

--- server/src/gameplay/catchFood/CatchFoodGame.ts
+++ server/src/gameplay/catchFood/CatchFoodGame.ts
@@ -1,227 +1,204 @@
 import {
-  PlayerState,
-  Obstacle,
-  // ObstacleType,
-  GameStateInfo,
-} from "./interfaces";
-import {
-  HashTable,
-  GameEventTypes,
-  GameState,
-  GameInterface,
-} from "../interfaces";
-import { User } from "../../interfaces/interfaces";
-import GameEventEmitter from "../../classes/GameEventEmitter";
-import { verifyGameState } from "../helperFunctions/verifyGameState";
-import { verifyUserId } from "../helperFunctions/verifyUserId";
-import { initiatePlayersState } from "./initiatePlayerState";
+    PlayerState,
+    Obstacle,
+    // ObstacleType,
+    GameStateInfo,
+} from './interfaces'
+import { HashTable, GameEventTypes, GameState, GameInterface } from '../interfaces'
+import { User } from '../../interfaces/interfaces'
+import GameEventEmitter from '../../classes/GameEventEmitter'
+import { verifyGameState } from '../helperFunctions/verifyGameState'
+import { verifyUserId } from '../helperFunctions/verifyUserId'
+import { initiatePlayersState } from './initiatePlayerState'
 
 interface CatchFoodGameInterface extends GameInterface {
-  playersState: HashTable<PlayerState>;
-  trackLength: number;
-  numberOfObstacles: number;
-
-  getGameStateInfo(): GameStateInfo;
-  getObstaclePositions(): HashTable<Array<Obstacle>>;
-  runForward(userId: string, speed: number): void;
-  playerHasCompletedObstacle(userId: string): void;
-  resetGame(
-    players: Array<User>,
-    trackLength: number,
+    playersState: HashTable<PlayerState>
+    trackLength: number
     numberOfObstacles: number
-  ): void;
+
+    getGameStateInfo(): GameStateInfo
+    getObstaclePositions(): HashTable<Array<Obstacle>>
+    runForward(userId: string, speed: number): void
+    playerHasCompletedObstacle(userId: string): void
+    resetGame(players: Array<User>, trackLength: number, numberOfObstacles: number): void
 }
 
 export default class CatchFoodGame implements CatchFoodGameInterface {
-  playersState: HashTable<PlayerState>;
-  trackLength: number;
-  numberOfObstacles: number;
-  currentRank: number;
-  gameEventEmitter: GameEventEmitter;
-  roomId: string;
-  gameState: GameState;
-  gameStartedTime: number;
-  players: Array<User>;
-  // timeOutLimit: number;
+    playersState: HashTable<PlayerState>
+    trackLength: number
+    numberOfObstacles: number
+    currentRank: number
+    gameEventEmitter: GameEventEmitter
+    roomId: string
+    gameState: GameState
+    gameStartedTime: number
+    players: Array<User>
+    // timeOutLimit: number;
 
-  constructor(players: Array<User>, trackLength = 2000, numberOfObstacles = 2) {
-    this.gameEventEmitter = GameEventEmitter.getInstance();
-    this.roomId = players[0].roomId;
-    this.gameState = GameState.Created;
-    this.trackLength = trackLength;
-    this.numberOfObstacles = numberOfObstacles;
-    this.currentRank = 1;
-    this.players = players;
-    this.playersState = initiatePlayersState(
-      players,
-      this.numberOfObstacles,
-      this.trackLength
-    );
-    this.gameStartedTime = 0;
-    // this.timeOutLimit = 300000;
-  }
-
-  startGame(): void {
-    try {
-      verifyGameState(this.gameState, GameState.Created);
-      this.gameState = GameState.Started;
-      this.gameEventEmitter.emit(GameEventTypes.GameHasStarted, {
-        roomId: this.roomId,
-        countdownTime: 3000,
-      });
-      this.gameStartedTime = Date.now();
-      // setInterval(this.onTimerTick, 33);
-    } catch (e) {
-      // throw e.Message;
-    }
-  }
-
-  // private onTimerTick() {
-  //   // console.log(Date.now() - this.gameStartedTime)
-  //   if (Date.now() - this.gameStartedTime > this.timeOutLimit) {
-  //     //300000ms = 5 min
-  //   }
-  // }
-
-  stopGame(): void {
-    try {
-      verifyGameState(this.gameState, GameState.Started);
-      this.gameState = GameState.Stopped;
-    } catch (e) {
-      // throw e.Message;
-      // console.error(e.message);
-    }
-  }
-
-  getGameStateInfo(): GameStateInfo {
-    const playerInfoArray = [];
-
-    for (const [_, playerState] of Object.entries(this.playersState)) {
-      playerInfoArray.push(playerState);
+    constructor(players: Array<User>, trackLength = 2000, numberOfObstacles = 2) {
+        this.gameEventEmitter = GameEventEmitter.getInstance()
+        this.roomId = players[0].roomId
+        this.gameState = GameState.Created
+        this.trackLength = trackLength
+        this.numberOfObstacles = numberOfObstacles
+        this.currentRank = 1
+        this.players = players
+        this.playersState = initiatePlayersState(players, this.numberOfObstacles, this.trackLength)
+        this.gameStartedTime = 0
+        // this.timeOutLimit = 300000;
     }
 
-    return {
-      gameState: this.gameState,
-      roomId: this.roomId,
-      playersState: playerInfoArray,
-      trackLength: this.trackLength,
-      numberOfObstacles: this.numberOfObstacles,
-    };
-  }
-
-  getObstaclePositions(): HashTable<Array<Obstacle>> {
-    const obstaclePositions: HashTable<Array<Obstacle>> = {};
-    for (const [_, playerState] of Object.entries(this.playersState)) {
-      obstaclePositions[playerState.id] = playerState.obstacles;
+    startGame(): void {
+        try {
+            verifyGameState(this.gameState, GameState.Created)
+            this.gameState = GameState.Started
+            this.gameEventEmitter.emit(GameEventTypes.GameHasStarted, {
+                roomId: this.roomId,
+                countdownTime: 3000,
+            })
+            this.gameStartedTime = Date.now()
+            // setInterval(this.onTimerTick, 33);
+        } catch (e) {
+            // throw e.Message;
+        }
     }
 
-    return obstaclePositions;
-  }
+    // private onTimerTick() {
+    //   // console.log(Date.now() - this.gameStartedTime)
+    //   if (Date.now() - this.gameStartedTime > this.timeOutLimit) {
+    //     //300000ms = 5 min
+    //   }
+    // }
 
-  runForward(userId: string, speed = 1): void {
-    try {
-      verifyUserId(this.playersState, userId);
-      verifyGameState(this.gameState, GameState.Started);
-
-      if (this.playersState[userId].finished) return;
-      if (this.playersState[userId].atObstacle) return;
-
-      this.playersState[userId].positionX += speed;
-
-      if (this.playerHasReachedObstacle(userId))
-        this.handlePlayerReachedObstacle(userId);
-
-      if (this.playerHasPassedGoal(userId)) this.playerHasFinishedGame(userId);
-    } catch (e) {
-      // throw e;
-      // console.error(e.message);
+    stopGame(): void {
+        try {
+            verifyGameState(this.gameState, GameState.Started)
+            this.gameState = GameState.Stopped
+        } catch (e) {
+            // throw e.Message;
+            // console.error(e.message);
+        }
     }
-  }
 
-  private playerHasReachedObstacle(userId: string): boolean {
-    return (
-      this.playersState[userId].obstacles.length > 0 &&
-      this.playersState[userId].positionX >=
-        this.playersState[userId].obstacles[0].positionX
-    );
-  }
+    getGameStateInfo(): GameStateInfo {
+        const playerInfoArray = []
 
-  private playerHasPassedGoal(userId: string): boolean {
-    return this.playersState[userId].positionX >= this.trackLength;
-  }
+        for (const [, playerState] of Object.entries(this.playersState)) {
+            playerInfoArray.push(playerState)
+        }
 
-  private handlePlayerReachedObstacle(userId: string): void {
-    // block player from running when obstacle is reached
-    this.playersState[userId].atObstacle = true;
-    this.gameEventEmitter.emit(GameEventTypes.ObstacleReached, {
-      roomId: this.roomId,
-      userId,
-      obstacleType: this.playersState[userId].obstacles[0].type,
-    });
-  }
-
-  playerHasCompletedObstacle(userId: string): void {
-    //TODO CHange to stop cheating
-    try {
-      verifyUserId(this.playersState, userId);
-      verifyGameState(this.gameState, GameState.Started);
-      //TODO: BLOCK USER FROM SAYING COMPLETED STRAIGHT AWAY - STOP CHEATING
-      this.playersState[userId].atObstacle = false;
-      this.playersState[userId].obstacles.shift();
-    } catch (e) {
-      // throw e.Message;
-      // console.error(e.message);
+        return {
+            gameState: this.gameState,
+            roomId: this.roomId,
+            playersState: playerInfoArray,
+            trackLength: this.trackLength,
+            numberOfObstacles: this.numberOfObstacles,
+        }
     }
-  }
 
-  private playerHasFinishedGame(userId: string): void {
-    //only if player hasn't already been marked as finished
-    if (this.playersState[userId].finished) return;
+    getObstaclePositions(): HashTable<Array<Obstacle>> {
+        const obstaclePositions: HashTable<Array<Obstacle>> = {}
+        for (const [, playerState] of Object.entries(this.playersState)) {
+            obstaclePositions[playerState.id] = playerState.obstacles
+        }
 
-    this.playersState[userId].finished = true;
-    this.playersState[userId].rank = this.currentRank++;
-
-    this.gameEventEmitter.emit(GameEventTypes.PlayerHasFinished, {
-      userId,
-      roomId: this.roomId,
-      rank: this.playersState[userId].rank,
-    });
-
-    if (this.gameHasFinished()) {
-      this.handleGameFinished();
+        return obstaclePositions
     }
-  }
 
-  private gameHasFinished(): boolean {
-    return this.currentRank > Object.keys(this.playersState).length;
-  }
+    runForward(userId: string, speed = 1): void {
+        try {
+            verifyUserId(this.playersState, userId)
+            verifyGameState(this.gameState, GameState.Started)
 
-  private handleGameFinished(): void {
-    this.gameState = GameState.Finished;
-    const currentGameStateInfo = this.getGameStateInfo();
-    this.gameEventEmitter.emit(GameEventTypes.GameHasFinished, {
-      roomId: currentGameStateInfo.roomId,
-      playersState: currentGameStateInfo.playersState,
-      gameState: currentGameStateInfo.gameState,
-      trackLength: currentGameStateInfo.trackLength,
-      numberOfObstacles: currentGameStateInfo.numberOfObstacles,
-    });
-    //Broadcast, stop game, return ranks
-  }
+            if (this.playersState[userId].finished) return
+            if (this.playersState[userId].atObstacle) return
 
-  resetGame(
-    players = this.players,
-    trackLength = 2000,
-    numberOfObstacles = 4
-  ): void {
-    this.gameState = GameState.Created;
-    this.trackLength = trackLength;
-    this.numberOfObstacles = numberOfObstacles;
-    this.currentRank = 1;
-    this.playersState = initiatePlayersState(
-      players,
-      this.numberOfObstacles,
-      this.trackLength
-    );
-  }
+            this.playersState[userId].positionX += speed
+
+            if (this.playerHasReachedObstacle(userId)) this.handlePlayerReachedObstacle(userId)
+
+            if (this.playerHasPassedGoal(userId)) this.playerHasFinishedGame(userId)
+        } catch (e) {
+            // throw e;
+            // console.error(e.message);
+        }
+    }
+
+    private playerHasReachedObstacle(userId: string): boolean {
+        return (
+            this.playersState[userId].obstacles.length > 0 &&
+            this.playersState[userId].positionX >= this.playersState[userId].obstacles[0].positionX
+        )
+    }
+
+    private playerHasPassedGoal(userId: string): boolean {
+        return this.playersState[userId].positionX >= this.trackLength
+    }
+
+    private handlePlayerReachedObstacle(userId: string): void {
+        // block player from running when obstacle is reached
+        this.playersState[userId].atObstacle = true
+        this.gameEventEmitter.emit(GameEventTypes.ObstacleReached, {
+            roomId: this.roomId,
+            userId,
+            obstacleType: this.playersState[userId].obstacles[0].type,
+        })
+    }
+
+    playerHasCompletedObstacle(userId: string): void {
+        //TODO CHange to stop cheating
+        try {
+            verifyUserId(this.playersState, userId)
+            verifyGameState(this.gameState, GameState.Started)
+            //TODO: BLOCK USER FROM SAYING COMPLETED STRAIGHT AWAY - STOP CHEATING
+            this.playersState[userId].atObstacle = false
+            this.playersState[userId].obstacles.shift()
+        } catch (e) {
+            // throw e.Message;
+            // console.error(e.message);
+        }
+    }
+
+    private playerHasFinishedGame(userId: string): void {
+        //only if player hasn't already been marked as finished
+        if (this.playersState[userId].finished) return
+
+        this.playersState[userId].finished = true
+        this.playersState[userId].rank = this.currentRank++
+
+        this.gameEventEmitter.emit(GameEventTypes.PlayerHasFinished, {
+            userId,
+            roomId: this.roomId,
+            rank: this.playersState[userId].rank,
+        })
+
+        if (this.gameHasFinished()) {
+            this.handleGameFinished()
+        }
+    }
+
+    private gameHasFinished(): boolean {
+        return this.currentRank > Object.keys(this.playersState).length
+    }
+
+    private handleGameFinished(): void {
+        this.gameState = GameState.Finished
+        const currentGameStateInfo = this.getGameStateInfo()
+        this.gameEventEmitter.emit(GameEventTypes.GameHasFinished, {
+            roomId: currentGameStateInfo.roomId,
+            playersState: currentGameStateInfo.playersState,
+            gameState: currentGameStateInfo.gameState,
+            trackLength: currentGameStateInfo.trackLength,
+            numberOfObstacles: currentGameStateInfo.numberOfObstacles,
+        })
+        //Broadcast, stop game, return ranks
+    }
+
+    resetGame(players = this.players, trackLength = 2000, numberOfObstacles = 4): void {
+        this.gameState = GameState.Created
+        this.trackLength = trackLength
+        this.numberOfObstacles = numberOfObstacles
+        this.currentRank = 1
+        this.playersState = initiatePlayersState(players, this.numberOfObstacles, this.trackLength)
+    }
 }
--- server/tests/gameplay/catchFoodGame/eventEmitter.test.ts
+++ server/tests/gameplay/catchFoodGame/eventEmitter.test.ts
@@ -1,83 +1,82 @@
-import { CatchFoodGame } from "../../../src/gameplay";
-import GameEventEmitter from "../../../src/classes/GameEventEmitter";
-import { GameEventTypes } from "../../../src/gameplay/interfaces";
-import { users } from "../mockUsers";
+import { CatchFoodGame } from '../../../src/gameplay'
+import GameEventEmitter from '../../../src/classes/GameEventEmitter'
+import { GameEventTypes } from '../../../src/gameplay/interfaces'
+import { users } from '../mockUsers'
 
-const TRACKLENGTH = 500;
-const NUMBER_OF_OBSTACLES = 4;
-let catchFoodGame: CatchFoodGame;
-let gameEventEmitter: GameEventEmitter;
+const TRACKLENGTH = 500
+const NUMBER_OF_OBSTACLES = 4
+let catchFoodGame: CatchFoodGame
+let gameEventEmitter: GameEventEmitter
 
-describe("Event Emitter", () => {
-  beforeAll(() => {
-    gameEventEmitter = GameEventEmitter.getInstance();
-  });
+describe('Event Emitter', () => {
+    beforeAll(() => {
+        gameEventEmitter = GameEventEmitter.getInstance()
+    })
 
-  beforeEach(() => {
-    catchFoodGame = new CatchFoodGame(users, TRACKLENGTH, NUMBER_OF_OBSTACLES);
-  });
+    beforeEach(() => {
+        catchFoodGame = new CatchFoodGame(users, TRACKLENGTH, NUMBER_OF_OBSTACLES)
+    })
 
-  it("should emit a GameHasStarted event when the game is started", async () => {
-    //Game started
-    let gameStartedEvent = false;
-    gameEventEmitter.on(GameEventTypes.GameHasStarted, () => {
-      gameStartedEvent = true;
-    });
-    expect(gameStartedEvent).toBeFalsy();
-    catchFoodGame.startGame();
-    await setTimeout(() => ({}), 100);
-    expect(gameStartedEvent).toBeTruthy();
-  });
+    it('should emit a GameHasStarted event when the game is started', async () => {
+        //Game started
+        let gameStartedEvent = false
+        gameEventEmitter.on(GameEventTypes.GameHasStarted, () => {
+            gameStartedEvent = true
+        })
+        expect(gameStartedEvent).toBeFalsy()
+        catchFoodGame.startGame()
+        await setTimeout(() => ({}), 100)
+        expect(gameStartedEvent).toBeTruthy()
+    })
 
-  it("should emit an ObstacleReached event when an obstacle is reached", async () => {
-    catchFoodGame.startGame();
+    it('should emit an ObstacleReached event when an obstacle is reached', async () => {
+        catchFoodGame.startGame()
 
-    let obstacleEventReceived = false;
-    gameEventEmitter.on(GameEventTypes.ObstacleReached, () => {
-      obstacleEventReceived = true;
-    });
+        let obstacleEventReceived = false
+        gameEventEmitter.on(GameEventTypes.ObstacleReached, () => {
+            obstacleEventReceived = true
+        })
 
-    const distanceToObstacle =
-      catchFoodGame.playersState["1"].obstacles[0].positionX -
-      catchFoodGame.playersState["1"].positionX;
-    catchFoodGame.runForward("1", distanceToObstacle);
-    expect(obstacleEventReceived).toBeTruthy();
-  });
+        const distanceToObstacle =
+            catchFoodGame.playersState['1'].obstacles[0].positionX - catchFoodGame.playersState['1'].positionX
+        catchFoodGame.runForward('1', distanceToObstacle)
+        expect(obstacleEventReceived).toBeTruthy()
+    })
 
-  it("should emit an PlayerHasFinished event when a player has reached the end of the race", async () => {
-    catchFoodGame.startGame();
-    // finish player 1
-    for (let i = 0; i < 4; i++) {
-      catchFoodGame.playerHasCompletedObstacle("1");
-    }
-    //
-    const gameEventEmitter = GameEventEmitter.getInstance();
-    let playerFinished = false;
-    gameEventEmitter.on(GameEventTypes.PlayerHasFinished, () => {
-      playerFinished = true;
-    });
-    catchFoodGame.runForward("1", 500);
-    expect(playerFinished).toBeTruthy();
-  });
+    it('should emit an PlayerHasFinished event when a player has reached the end of the race', async () => {
+        catchFoodGame.startGame()
+        // finish player 1
+        for (let i = 0; i < 4; i++) {
+            catchFoodGame.playerHasCompletedObstacle('1')
+        }
+        //
+        const gameEventEmitter = GameEventEmitter.getInstance()
+        let playerFinished = false
+        gameEventEmitter.on(GameEventTypes.PlayerHasFinished, () => {
+            playerFinished = true
+        })
+        catchFoodGame.runForward('1', 500)
+        expect(playerFinished).toBeTruthy()
+    })
 
-  it("should emit a PlayerHasFinished event when a all the players have finished race", async () => {
-    catchFoodGame.startGame();
-    let gameFinishedEvent = false;
-    gameEventEmitter.on(GameEventTypes.PlayerHasFinished, () => {
-      gameFinishedEvent = true;
-    });
-    // finish game
-    for (let i = 0; i < 4; i++) {
-      catchFoodGame.playerHasCompletedObstacle("1");
-      catchFoodGame.playerHasCompletedObstacle("2");
-      catchFoodGame.playerHasCompletedObstacle("3");
-      catchFoodGame.playerHasCompletedObstacle("4");
-    }
+    it('should emit a GameHasFinished event when a all the players have finished race', async () => {
+        catchFoodGame.startGame()
+        let gameFinishedEvent = false
+        gameEventEmitter.on(GameEventTypes.GameHasFinished, () => {
+            gameFinishedEvent = true
+        })
+        // finish game
+        for (let i = 0; i < 4; i++) {
+            catchFoodGame.playerHasCompletedObstacle('1')
+            catchFoodGame.playerHasCompletedObstacle('2')
+            catchFoodGame.playerHasCompletedObstacle('3')
+            catchFoodGame.playerHasCompletedObstacle('4')
+        }
 
-    catchFoodGame.runForward("1", 500);
-    catchFoodGame.runForward("2", 500);
-    catchFoodGame.runForward("3", 500);
-    catchFoodGame.runForward("4", 500);
-    expect(gameFinishedEvent).toBeTruthy();
-  });
-});
+        catchFoodGame.runForward('1', 500)
+        catchFoodGame.runForward('2', 500)
+        catchFoodGame.runForward('3', 500)
+        catchFoodGame.runForward('4', 500)
+        expect(gameFinishedEvent).toBeTruthy()
+    })
+})
