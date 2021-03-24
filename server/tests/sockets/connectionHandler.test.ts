28b4ffae4709f113820a543404c200576a415c03
Natasha Troth <ntroth.mmt-m2020@fh-salzburg.ac.at>
2021-03-24 15:55:22 +0100

    Fix connection handler test

--- server/tests/sockets/connectionHandler.test.ts
+++ server/tests/sockets/connectionHandler.test.ts
@@ -20,7 +20,8 @@ describe('connectionHandler', () => {
 
     beforeAll(done => {
         server = new HttpServer()
-        const PORT = process.env.PORT || 5001
+
+        const PORT = process.env.PORT || 5050
         expresServer = server.app.listen({ port: PORT })
         io = require('socket.io')(expresServer, {
             cors: {
