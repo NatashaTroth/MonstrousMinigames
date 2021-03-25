15f4620c0fdf63767f433cafc087d7af00c9568f
Natasha Troth <ntroth.mmt-m2020@fh-salzburg.ac.at>
2021-03-25 15:35:13 +0100

    Remove playersrank for semicolon merge

--- server/src/gameplay/catchFood/interfaces/GameEvents.ts
+++ server/src/gameplay/catchFood/interfaces/GameEvents.ts
@@ -1,6 +1,7 @@
 import { GameState } from '../../interfaces';
 import { ObstacleType, PlayerState } from './';
-import { PlayerRank } from './PlayerRank';
+
+// import { PlayerRank } from './PlayerRank';
 
 interface GameEventInterface {
     roomId: string
@@ -17,7 +18,7 @@ export interface GameHasFinished extends GameEventInterface {
     gameState: GameState
     trackLength: number
     numberOfObstacles: number
-    playerRanks: Array<PlayerRank>
+    // playerRanks: Array<PlayerRank>
 }
 
 export interface GameHasStopped extends GameEventInterface {
