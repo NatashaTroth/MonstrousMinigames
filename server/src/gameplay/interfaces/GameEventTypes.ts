63025da67028cec551ae727fd8a6136ea93ad295
Natasha Troth <ntroth.mmt-m2020@fh-salzburg.ac.at>
2021-03-25 10:12:47 +0100

    Add gamestate to stop game timeout event and emit event when game is stopped

--- server/src/gameplay/interfaces/GameEventTypes.ts
+++ server/src/gameplay/interfaces/GameEventTypes.ts
@@ -2,6 +2,7 @@ export enum GameEventTypes {
     ObstacleReached = 'OBSTACLE_REACHED',
     PlayerHasFinished = 'PLAYER_HAS_FINISHED',
     GameHasStarted = 'GAME_HAS_STARTED',
+    GameHasStopped = 'GAME_HAS_STOPPED',
     GameHasFinished = 'GAME_HAS_FINISHED',
     GameHasTimedOut = 'GAME_HAS_TIMED_OUT',
 }
