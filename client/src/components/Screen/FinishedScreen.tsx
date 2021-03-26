32ce42dd15ff9207861d74724702cda108ee9149
Magdalena Maislinger <mmaislinger.mmt-m2020@fh-salzburg.ac.at>
2021-03-26 10:29:55 +0100

    WIP: display time at leaderboard

--- client/src/components/Screen/FinishedScreen.tsx
+++ client/src/components/Screen/FinishedScreen.tsx
@@ -1,22 +1,28 @@
 import * as React from 'react'
 
 import { GameContext } from '../../contexts/GameContextProvider'
+import { formatMs } from '../../utils/formatMs'
 import { FinishedScreenContainer, FinishedScreenText, Headline, RankTable } from './FinishedScreen.sc'
 
 export const FinishedScreen: React.FunctionComponent = () => {
-    const { players } = React.useContext(GameContext)
+    const { playerRanks } = React.useContext(GameContext)
+
+    const sortedPlayerRanks = playerRanks?.sort((a, b) => a.rank - b.rank)
 
     return (
         <FinishedScreenContainer>
             <RankTable>
                 <Headline>Finished!</Headline>
-                {players
-                    ?.sort((a, b) => a.rank - b.rank)
-                    .map(player => (
-                        <FinishedScreenText key={player.name}>
-                            #{player.rank} {player.name}
-                        </FinishedScreenText>
-                    ))}
+                {sortedPlayerRanks?.map((player, index) => (
+                    <FinishedScreenText key={player.name}>
+                        #{player.rank} {player.name}{' '}
+                        {formatMs(
+                            index === 0
+                                ? player.totalTimeInMs
+                                : player.totalTimeInMs - sortedPlayerRanks[0].totalTimeInMs
+                        )}
+                    </FinishedScreenText>
+                ))}
             </RankTable>
         </FinishedScreenContainer>
     )
