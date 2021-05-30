5b4e850fb7ff467282e4b6ebd726c9b975ecb1e0
Robin Fellinger <rfellinger.mmt-m2020@fh-salzburg.ac.at>
2021-05-30 16:00:03 +0200

    - start game from controller

--- server/src/services/connectionHandler.ts
+++ server/src/services/connectionHandler.ts
@@ -121,30 +121,6 @@ class ConnectionHandler {
             socket.on('message', function (message: IMessage) {
                 const type = message.type;
                 switch (type) {
-                    case CatchFoodMsgType.START: {
-                        if (socket.room.isOpen() && socket.room.isAdmin(socket.user)) {
-                            try {
-                                room.startGame();
-                            } catch (e) {
-                                console.error(socket.room.id + ' | ' + e.name);
-                                emitter.sendErrorMessage(socket, e);
-                            }
-
-                            emitter.sendGameState(screenNameSpace, socket.room);
-
-                            const gameStateInterval = setInterval(() => {
-                                if (!socket.room.isPlaying() && !socket.room.isPaused()) {
-                                    clearInterval(gameStateInterval);
-                                }
-                                // send gamestate volatile
-                                if (socket.room.isPlaying()) {
-                                    emitter.sendGameState(screenNameSpace, socket.room, true);
-                                }
-                            }, Globals.GAME_STATE_UPDATE_MS);
-                        }
-
-                        break;
-                    }
                     case CatchFoodMsgType.MOVE: {
                         if (socket.room.isPlaying()) {
                             try {
