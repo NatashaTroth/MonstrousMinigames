81af2f754d17b0ff88c82499650a8cf009f54986
Magdalena Maislinger <mmaislinger.mmt-m2020@fh-salzburg.ac.at>
2021-08-30 11:04:34 +0200

    Add new trash images

--- client/src/domain/controller/components/obstacles/Trash.tsx
+++ client/src/domain/controller/components/obstacles/Trash.tsx
@@ -7,21 +7,13 @@ import Button from '../../../../components/common/Button';
 import { ControllerSocketContext } from '../../../../contexts/ControllerSocketContextProvider';
 import { GameContext } from '../../../../contexts/GameContextProvider';
 import { PlayerContext } from '../../../../contexts/PlayerContextProvider';
+import food from '../../../../images/obstacles/trash/food.svg';
+import paper from '../../../../images/obstacles/trash/paper.svg';
+import plastic from '../../../../images/obstacles/trash/plastic.svg';
 import { dragMoveListener, initializeInteractListeners, itemCounter } from './Draggable';
 import LinearProgressBar from './LinearProgressBar';
 import { ObstacleContainer } from './ObstaclStyles.sc';
-import {
-    Container,
-    Draggable,
-    DropZone,
-    StyledBattery,
-    StyledFood,
-    StyledPaper,
-    StyledSkipButton,
-    TrashBattery,
-    TrashFood,
-    TrashPaper,
-} from './Trash.sc';
+import { Container, Draggable, DropZone, StyledImage, StyledSkipButton } from './Trash.sc';
 
 interface WindowProps extends Window {
     dragMoveListener?: unknown;
@@ -29,16 +21,18 @@ interface WindowProps extends Window {
 
 let MAX = 0;
 
+export type TrashType = 'paper' | 'food' | 'plastic';
+
 const Trash: React.FunctionComponent = () => {
     const [skip, setSkip] = React.useState(false);
     const { obstacle, setObstacle } = React.useContext(PlayerContext);
     const { controllerSocket } = React.useContext(ControllerSocketContext);
     const { roomId } = React.useContext(GameContext);
     const [randomArray, setRandomArray] = React.useState<number[]>([]);
-    const [actualItem, setActualItem] = React.useState<string | undefined>();
+    const [actualItem, setActualItem] = React.useState<TrashType | undefined>();
     const [progress, setProgress] = React.useState(0);
 
-    const items = ['paper', 'food', 'battery'];
+    const items: TrashType[] = ['paper', 'food', 'plastic'];
 
     let handleSkip: ReturnType<typeof setTimeout>;
 
@@ -108,39 +102,30 @@ const Trash: React.FunctionComponent = () => {
                             case 'paper':
                                 return (
                                     <Draggable index={index} key={`draggable${index}`} id="paper" className="drag-drop">
-                                        <StyledPaper />
+                                        <StyledImage src={paper} />
                                     </Draggable>
                                 );
                             case 'food':
                                 return (
                                     <Draggable index={index} key={`draggable${index}`} id="food" className="drag-drop">
-                                        <StyledFood />
+                                        <StyledImage src={food} />
                                     </Draggable>
                                 );
-                            case 'battery':
+                            case 'plastic':
                                 return (
                                     <Draggable
                                         index={index}
                                         key={`draggable${index}`}
-                                        id="battery"
+                                        id="plastic"
                                         className="drag-drop"
                                     >
-                                        <StyledBattery />
+                                        <StyledImage src={plastic} />
                                     </Draggable>
                                 );
                         }
                     })}
 
-                    <DropZone className="dropzone">
-                        {actualItem &&
-                            (actualItem === 'paper' ? (
-                                <TrashPaper />
-                            ) : actualItem === 'food' ? (
-                                <TrashFood />
-                            ) : (
-                                <TrashBattery />
-                            ))}
-                    </DropZone>
+                    {actualItem && <DropZone className="dropzone" variant={actualItem} />}
                 </Container>
                 {skip && (
                     <StyledSkipButton>
