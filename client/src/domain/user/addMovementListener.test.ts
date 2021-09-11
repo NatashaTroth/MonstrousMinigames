b787061284b39f50a45052c802895c5e65b13214
Magdalena Maislinger <mmaislinger.mmt-m2020@fh-salzburg.ac.at>
2021-09-11 16:32:55 +0200

    Remove addEventlistener tests

--- client/src/domain/user/addMovementListener.test.ts
+++ client/src/domain/user/addMovementListener.test.ts
@@ -1,6 +1,5 @@
 import { InMemorySocketFake } from '../socket/InMemorySocketFake';
-import { Window } from '../window/Window';
-import addMovementListener, { sendMovementToController } from './addMovementListener';
+import { sendMovementToController } from './addMovementListener';
 
 describe('test sendMovementToController function', () => {
     it('sendMovementToController should call handed sendMovement function', async () => {
@@ -17,23 +16,5 @@ describe('test sendMovementToController function', () => {
 
         sendMovementToController(e, playerFinished, controllerSocket, hasPaused, sendMovement);
         expect(sendMovement).toHaveBeenCalledTimes(1);
-    });
-});
-
-describe('test addMovementListener function', () => {
-    it('addMovementListener should call handed window eventlistener', async () => {
-        const e = {
-            preventDefault: jest.fn(),
-            acceleration: {
-                x: 5,
-            },
-        };
-        const playerFinished = false;
-        const hasPaused = false;
-        const controllerSocket = new InMemorySocketFake();
-        const handleEvent = jest.fn();
-
-        addMovementListener(controllerSocket, hasPaused, playerFinished);
-        expect(handleEvent).toHaveBeenCalledTimes(1);
     });
 });
