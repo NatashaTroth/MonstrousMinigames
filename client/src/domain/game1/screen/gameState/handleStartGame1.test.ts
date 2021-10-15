9d4d7cae9b593a5e4074861c2eff1c4f0b2e8a78
Magdalena Maislinger <mmaislinger.mmt-m2020@fh-salzburg.ac.at>
2021-10-15 14:05:57 +0200

    Reach 70% frontend test coverage

---
+++ client/src/domain/game1/screen/gameState/handleStartGame1.test.ts
@@ -0,0 +1,24 @@
+import { MessageTypesGame1 } from '../../../../utils/constants';
+import { InMemorySocketFake } from '../../../socket/InMemorySocketFake';
+import handleStartGame1 from './handleStartGame1';
+
+describe('handleStartGame1', () => {
+    it('startPhaserGame should be emitted to socket', () => {
+        const screenSocket = new InMemorySocketFake();
+        const roomId = 'ABCD';
+        const userId = '1';
+
+        global.sessionStorage.setItem('roomId', roomId);
+        global.sessionStorage.setItem('userId', userId);
+
+        handleStartGame1(screenSocket);
+
+        expect(screenSocket.emitedVals).toStrictEqual([
+            {
+                type: MessageTypesGame1.startPhaserGame,
+                roomId,
+                userId,
+            },
+        ]);
+    });
+});
