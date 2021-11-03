5b39ed30d000d9580146781fbf9f28eef5c025b2
Natasha Troth <ntroth.mmt-m2020@fh-salzburg.ac.at>
2021-11-03 20:48:29 +0100

    Refactor resetGame tests

--- server/tests/gameplay/gameOne/unit/resetGame.test.ts
+++ server/tests/gameplay/gameOne/unit/resetGame.test.ts
@@ -1,10 +1,8 @@
 import 'reflect-metadata';
 
 import { GameOne } from '../../../../src/gameplay';
+import { GameState } from '../../../../src/gameplay/enums';
 import { leaderboard, roomId, users } from '../../mockData';
-import {
-    clearTimersAndIntervals, releaseThreadN, startAndFinishGame
-} from './gameOneHelperFunctions';
 
 const NEW_TRACKLENGTH = 1000;
 const NEW_NUMBER_OF_OBSTACLES = 6;
@@ -15,14 +13,9 @@ describe('Create new game tests', () => {
     beforeEach(async () => {
         gameOne = new GameOne(roomId, leaderboard);
         jest.useFakeTimers();
-        // console.log(gameOne.players.get('1'));
-
-        startAndFinishGame(gameOne);
-        await releaseThreadN(3);
+        gameOne.createNewGame(users);
+        gameOne.gameState = GameState.Finished;
         gameOne.createNewGame(users, NEW_TRACKLENGTH, NEW_NUMBER_OF_OBSTACLES, NEW_STONE_COUNT);
-    });
-    afterEach(async () => {
-        clearTimersAndIntervals(gameOne);
     });
 
     it('should have the correct new number of players', async () => {
