4ca5f2aeabb16825a88bebe877b6a9c54bf3d20b
Magdalena Maislinger <mmaislinger.mmt-m2020@fh-salzburg.ac.at>
2021-11-21 13:32:59 +0100

    Handle back to lobby

--- client/src/tests/controller/gameState/handleGameHasResetMessage.test.ts
+++ client/src/tests/controller/gameState/handleGameHasResetMessage.test.ts
@@ -1,14 +1,17 @@
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory } from "history";
 
-import { handleGameHasResetMessage } from '../../../domain/commonGameState/controller/handleGameHasResetMessage';
+import {
+    handleGameHasResetMessage
+} from "../../../domain/commonGameState/controller/handleGameHasResetMessage";
 
 describe('handleGameHasResetMessage', () => {
     const roomId = '1234';
+    const resetController = jest.fn();
 
     it('history push should be called with handed roomId', () => {
         const history = createMemoryHistory();
 
-        handleGameHasResetMessage(history, roomId);
+        handleGameHasResetMessage(history, roomId, resetController);
 
         expect(history.location).toHaveProperty('pathname', `/controller/${roomId}/lobby`);
     });
