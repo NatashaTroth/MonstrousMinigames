d0b1462ce0e045e130c3d3a4ed6b255d72649f2e
Robin Fellinger <rfellinger.mmt-m2020@fh-salzburg.ac.at>
2021-11-25 13:20:10 +0100

    + game two: add player directions

--- server/src/gameplay/gameTwo/GameTwoPlayer.ts
+++ server/src/gameplay/gameTwo/GameTwoPlayer.ts
@@ -21,9 +21,21 @@ class GameTwoPlayer extends Player {
     update(timeElapsed: number, timeElapsedSinceLastFrame: number): void | Promise<void> {
         if (this.direction !== Direction.STOP) {
             switch (this.direction) {
+                case Direction.UP_LEFT:
+                    if (this.posY > 0 && this.posX > 0) {
+                        this.posY -= this.speed;
+                        this.posX -= this.speed;
+                    }
+                    break;
                 case Direction.UP:
                     if (this.posY > 0) {
                         this.posY -= this.speed;
+                    }
+                    break;
+                case Direction.UP_RIGHT:
+                    if (this.posY > 0 && this.posX < InitialParameters.LENGTH_X) {
+                        this.posY -= this.speed;
+                        this.posX += this.speed;
                     }
                     break;
                 case Direction.RIGHT:
@@ -31,9 +43,21 @@ class GameTwoPlayer extends Player {
                         this.posX += this.speed;
                     }
                     break;
+                case Direction.DOWN_RIGHT:
+                    if (this.posY < InitialParameters.LENGTH_Y && this.posX < InitialParameters.LENGTH_X) {
+                        this.posY += this.speed;
+                        this.posX += this.speed;
+                    }
+                    break;
                 case Direction.DOWN:
                     if (this.posY < InitialParameters.LENGTH_Y) {
                         this.posY += this.speed;
+                    }
+                    break;
+                case Direction.DOWN_LEFT:
+                    if (this.posY < InitialParameters.LENGTH_Y && this.posX > 0) {
+                        this.posY += this.speed;
+                        this.posX -= this.speed;
                     }
                     break;
                 case Direction.LEFT:
--- server/src/gameplay/gameTwo/enums/Direction.ts
+++ server/src/gameplay/gameTwo/enums/Direction.ts
@@ -1,7 +1,11 @@
 export enum Direction {
     UP = 'N',
+    UP_RIGHT = 'NE',
+    UP_LEFT = 'NW',
     RIGHT = 'E',
     DOWN = 'S',
+    DOWN_RIGHT = 'SE',
+    DOWN_LEFT = 'SW',
     LEFT = 'W',
     STOP = 'C'
 }
