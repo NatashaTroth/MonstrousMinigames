d6289bb307620bd07afcdc5ff6b8a07b87e4e79f
Magdalena Maislinger <mmaislinger.mmt-m2020@fh-salzburg.ac.at>
2021-11-25 20:01:59 +0100

    Refactor controller socket handler

--- client/src/tests/socket/handleSocketConnectionController.test.ts
+++ client/src/tests/socket/handleSocketConnectionController.test.ts
@@ -1,42 +1,39 @@
 import { createMemoryHistory } from 'history';
 
-import { handleSocketConnection } from '../../domain/socket/controller/handleSocketConnection';
+import {
+    HandleSocketConnDependencies,
+    handleSocketConnection,
+} from '../../domain/socket/controller/handleSocketConnection';
 
 describe('Controller Socket Connection', () => {
     it('handed setRoomId function should be called with handed roomId', () => {
         const roomId = 'ABCD';
         const history = createMemoryHistory();
         const setRoomId = jest.fn();
-        const dependencies = {
-            setRoomId,
-            setControllerSocket: jest.fn(),
-            setPlayerNumber: jest.fn(),
-            setPlayerFinished: jest.fn(),
-            setObstacle: jest.fn(),
-            setPlayerRank: jest.fn(),
-            setHasPaused: jest.fn(),
-            setGameStarted: jest.fn(),
-            setName: jest.fn(),
-            setAvailableCharacters: jest.fn(),
-            setUserId: jest.fn(),
-            setReady: jest.fn(),
-            setPlayerDead: jest.fn(),
+        const dependencies: HandleSocketConnDependencies = {
             history,
-            setConnectedUsers: jest.fn(),
-            playerRank: undefined,
-            setEarlySolvableObstacle: jest.fn(),
-            setExceededChaserPushes: jest.fn(),
-            setStunnablePlayers: jest.fn(),
+            setRoomId: jest.fn(),
             setChosenGame: jest.fn(),
-            setVoteForPhotoMessage: jest.fn(),
-            setRoundIdx: jest.fn(),
-            setCountdownTime: jest.fn(),
-            setTopicMessage: jest.fn(),
-            setSheepGameStarted: jest.fn(),
-            setVotingResults: jest.fn(),
+            setControllerSocket: jest.fn(),
+            setExceededChaserPushes: jest.fn(),
             setFinalRoundCountdownTime: jest.fn(),
+            setHasPaused: jest.fn(),
             setPresentFinalPhotos: jest.fn(),
-            resetController: jest.fn(),
+            setRoundIdx: jest.fn(),
+            setTopicMessage: jest.fn(),
+            setVoteForPhotoMessage: jest.fn(),
+            setVotingResults: jest.fn(),
+            handleApproachingObstacleMessage: jest.fn(),
+            handleConnectedUsersMessage: jest.fn(),
+            handleGameHasFinishedMessage: jest.fn(),
+            handleGameHasResetMessage: jest.fn(),
+            handleGameHasStoppedMessage: jest.fn(),
+            handleGameStartedMessage: jest.fn(),
+            handleObstacleMessage: jest.fn(),
+            handlePlayerDied: jest.fn(),
+            handlePlayerFinishedMessage: jest.fn(),
+            handleStunnablePlayers: jest.fn(),
+            handleUserInitMessage: jest.fn(),
         };
 
         handleSocketConnection(roomId, 'Test', false, dependencies);
