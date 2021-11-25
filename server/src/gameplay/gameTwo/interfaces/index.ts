0e06a2cb634f069d8220becd905fc4058549ef9e
Robin Fellinger <rfellinger.mmt-m2020@fh-salzburg.ac.at>
2021-11-25 09:31:20 +0100

    ~ fix import

--- server/src/gameplay/gameTwo/interfaces/index.ts
+++ server/src/gameplay/gameTwo/interfaces/index.ts
@@ -1,12 +1,9 @@
 import { GameTwoPlayerRank } from './GameTwoPlayerRank';
 import { GameStateInfo } from './GameStateInfo';
 import { PlayerStateForClient } from './PlayerStateForClient';
-import { Guess } from './Guess';
-
 
 export {
     PlayerStateForClient,
     GameStateInfo,
     GameTwoPlayerRank as PlayerRank,
-    Guess,
 };
