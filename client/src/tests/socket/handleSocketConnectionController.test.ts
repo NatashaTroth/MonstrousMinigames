0dbc346bf98afec17fb7e80fa60edeb377e14907
Magdalena Maislinger <mmaislinger.mmt-m2020@fh-salzburg.ac.at>
2021-12-03 12:01:33 +0100

    Fix tests

--- client/src/tests/socket/handleSocketConnectionController.test.ts
+++ client/src/tests/socket/handleSocketConnectionController.test.ts
@@ -1,5 +1,3 @@
-import { createMemoryHistory } from 'history';
-
 import {
     HandleSocketConnDependencies,
     handleSocketConnection,
@@ -8,32 +6,20 @@ import {
 describe('Controller Socket Connection', () => {
     it('handed setRoomId function should be called with handed roomId', () => {
         const roomId = 'ABCD';
-        const history = createMemoryHistory();
         const setRoomId = jest.fn();
         const dependencies: HandleSocketConnDependencies = {
-            history,
             setRoomId,
-            setChosenGame: jest.fn(),
-            setControllerSocket: jest.fn(),
             setExceededChaserPushes: jest.fn(),
             setFinalRoundCountdownTime: jest.fn(),
-            setHasPaused: jest.fn(),
             setPresentFinalPhotos: jest.fn(),
             setRoundIdx: jest.fn(),
             setTopicMessage: jest.fn(),
             setVoteForPhotoMessage: jest.fn(),
             setVotingResults: jest.fn(),
             handleApproachingObstacleMessage: jest.fn(),
-            handleConnectedUsersMessage: jest.fn(),
-            handleGameHasFinishedMessage: jest.fn(),
-            handleGameHasResetMessage: jest.fn(),
-            handleGameHasStoppedMessage: jest.fn(),
-            handleGameStartedMessage: jest.fn(),
-            handleObstacleMessage: jest.fn(),
             handlePlayerDied: jest.fn(),
             handlePlayerFinishedMessage: jest.fn(),
             handleStunnablePlayers: jest.fn(),
-            handleUserInitMessage: jest.fn(),
         };
 
         handleSocketConnection(roomId, 'Test', false, dependencies);
