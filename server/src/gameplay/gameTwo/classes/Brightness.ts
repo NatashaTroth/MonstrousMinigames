55fb02299e28f4aaddc756c71680785a58551a17
Robin Fellinger <rfellinger.mmt-m2020@fh-salzburg.ac.at>
2021-12-06 13:58:53 +0100

    ~ game two: brightness interval

--- server/src/gameplay/gameTwo/classes/Brightness.ts
+++ server/src/gameplay/gameTwo/classes/Brightness.ts
@@ -2,10 +2,32 @@ import Parameters from "../constants/Parameters";
 
 export default class Brightness {
     public brightness: number;
+    private interval: NodeJS.Timer | null;
+
     constructor() {
         this.brightness = 100;
+        this.interval = null;
     }
-    update(): void {
+    private update(): void {
         this.brightness -= Parameters.BRIGHTNESS_STEP;
+        if (this.interval && this.brightness >= 0) {
+            this.stop();
+        }
+    }
+
+    start(): void {
+        setTimeout(() => {
+            this.interval = setInterval(() => this.update(), Parameters.BRIGHTNESS_INTERVAL);
+
+        }, Parameters.BRIGHTNESS_TIMEOUT);
+    }
+
+
+    stop(): boolean {
+        if (this.interval) {
+            clearInterval(this.interval)
+            return true;
+        }
+        return false;
     }
 }
