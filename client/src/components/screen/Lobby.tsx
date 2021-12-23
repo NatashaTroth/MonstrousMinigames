4b9044bd2cd5f1e084400ca92aabc3279278b04a
Magdalena Maislinger <mmaislinger.mmt-m2020@fh-salzburg.ac.at>
2021-12-23 09:08:29 +0100

    Fullscreen QRCode

--- client/src/components/screen/Lobby.tsx
+++ client/src/components/screen/Lobby.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Assignment } from '@material-ui/icons';
+import { Dialog, IconButton } from '@material-ui/core';
+import { Assignment, ZoomOutMap } from '@material-ui/icons';
 import * as React from 'react';
 import { useParams } from 'react-router-dom';
 
@@ -24,11 +25,15 @@ import {
     ConnectedUserStatus,
     Content,
     ContentContainer,
+    ContentWrapper,
     CopyToClipboard,
+    DialogContent,
+    FullScreenIcon,
     LeftContainer,
     LobbyContainer,
     QRCode,
     QRCodeInstructions,
+    QRCodeWrapper,
     RightButtonContainer,
     RightContainer,
 } from './Lobby.sc';
@@ -41,8 +46,16 @@ export const Lobby: React.FunctionComponent = () => {
     const { changeSound } = React.useContext(MyAudioContext);
     const { id }: RouteParams = useParams();
     const navigator = window.navigator;
+    const [qRCodeDialogOpen, setQRCodeDialogOpen] = React.useState(false);
 
     React.useEffect(() => {
+        if (screenAdmin) {
+            screenSocket?.emit({
+                type: MessageTypes.screenState,
+                state: ScreenStates.lobby,
+            });
+        }
+
         changeSound(Sound.lobby);
 
         if (screenSocket) {
@@ -72,73 +85,109 @@ export const Lobby: React.FunctionComponent = () => {
         generateQRCode(`${process.env.REACT_APP_FRONTEND_URL}${roomId}`, 'qrCode');
     }, [roomId]);
 
-    React.useEffect(() => {
-        if (screenAdmin) {
-            screenSocket?.emit({
-                type: MessageTypes.screenState,
-                state: ScreenStates.lobby,
-            });
-        }
-    }, []);
+    function handleQRCodeFullscreen() {
+        setQRCodeDialogOpen(true);
+    }
 
     return (
-        <LobbyContainer>
-            <Content>
-                <LobbyHeader />
-                <ContentContainer>
-                    <LeftContainer>
-                        <ConnectedUsers>
-                            {getUserArray(connectedUsers || []).map((user, index) => (
-                                <ConnectedUserContainer key={`LobbyScreen${roomId}${user.number}`}>
-                                    <ConnectedUserCharacter number={user.number} free={user.free}>
-                                        <CharacterContainer>
-                                            {!user.free && user.characterNumber !== -1 && (
-                                                <Character src={characters[Number(user.characterNumber)].src} />
-                                            )}
-                                        </CharacterContainer>
+        <>
+            <LobbyContainer>
+                <Content>
+                    <LobbyHeader />
+                    <ContentContainer>
+                        <LeftContainer>
+                            <ConnectedUsers>
+                                {getUserArray(connectedUsers || []).map((user, index) => (
+                                    <ConnectedUserContainer key={`LobbyScreen${roomId}${user.number}`}>
+                                        <ConnectedUserCharacter number={user.number} free={user.free}>
+                                            <CharacterContainer>
+                                                {!user.free && user.characterNumber !== -1 && (
+                                                    <Character src={characters[Number(user.characterNumber)].src} />
+                                                )}
+                                            </CharacterContainer>
 
-                                        {user.free ? `Player ${user.number}` : user.name}
-                                    </ConnectedUserCharacter>
-                                    <ConnectedUserStatus number={user.number} free={user.free}>
-                                        {!user.free && (user.ready ? 'Ready' : 'Not Ready')}
-                                        {user.free && user.name}
-                                    </ConnectedUserStatus>
-                                </ConnectedUserContainer>
-                            ))}
-                        </ConnectedUsers>
-                    </LeftContainer>
-                    <RightContainer>
-                        <QRCode>
-                            <QRCodeInstructions>Scan QR-Code to add your mobile device:</QRCodeInstructions>
-                            <div id="qrCode" />
+                                            {user.free ? `Player ${user.number}` : user.name}
+                                        </ConnectedUserCharacter>
+                                        <ConnectedUserStatus number={user.number} free={user.free}>
+                                            {!user.free && (user.ready ? 'Ready' : 'Not Ready')}
+                                            {user.free && user.name}
+                                        </ConnectedUserStatus>
+                                    </ConnectedUserContainer>
+                                ))}
+                            </ConnectedUsers>
+                        </LeftContainer>
+                        <RightContainer>
+                            <QRCode>
+                                <FullScreenIcon>
+                                    <QRCodeInstructions>Scan QR-Code to add your mobile device:</QRCodeInstructions>
+                                    <IconButton onClick={handleQRCodeFullscreen} color="inherit">
+                                        <ZoomOutMap />
+                                    </IconButton>
+                                </FullScreenIcon>
+                                <div id="qrCode" />
+                                <CopyToClipboard onClick={handleCopyToClipboard}>
+                                    Copy to Clipboard
+                                    <Assignment />
+                                </CopyToClipboard>
+                            </QRCode>
+                            <RightButtonContainer>
+                                <Button
+                                    onClick={() => history.push(screenChooseGameRoute(roomId))}
+                                    disabled={!screenAdmin || !connectedUsers || connectedUsers?.length === 0}
+                                    variant="secondary"
+                                    title={
+                                        screenAdmin
+                                            ? !connectedUsers || connectedUsers?.length === 0
+                                                ? 'Wait for users to join'
+                                                : ''
+                                            : 'Please select the game on the admin screen'
+                                    }
+                                >
+                                    Choose Game
+                                </Button>
+
+                                <Button onClick={() => history.push(screenLeaderboardRoute(roomId))}>
+                                    Leaderboard
+                                </Button>
+                                <Button onClick={() => history.push(Routes.screen)}>Back</Button>
+                            </RightButtonContainer>
+                        </RightContainer>
+                    </ContentContainer>
+                </Content>
+            </LobbyContainer>
+            <Dialog
+                maxWidth="md"
+                open={qRCodeDialogOpen}
+                PaperProps={{
+                    style: {
+                        backgroundColor: 'transparent',
+                        boxShadow: 'none',
+                        width: '80%',
+                        height: '80%',
+                    },
+                }}
+                onEntered={() => {
+                    generateQRCode(`${process.env.REACT_APP_FRONTEND_URL}${roomId}`, 'qrCodeFullscreen');
+                }}
+                onClose={() => setQRCodeDialogOpen(false)}
+                fullScreen
+            >
+                <DialogContent>
+                    <ContentWrapper>
+                        <QRCode size="fullscreen">
+                            <QRCodeInstructions color="inherit">
+                                Scan QR-Code to add your mobile device:
+                            </QRCodeInstructions>
+                            <QRCodeWrapper id="qrCodeFullscreen" />
                             <CopyToClipboard onClick={handleCopyToClipboard}>
                                 Copy to Clipboard
                                 <Assignment />
                             </CopyToClipboard>
                         </QRCode>
-                        <RightButtonContainer>
-                            <Button
-                                onClick={() => history.push(screenChooseGameRoute(roomId))}
-                                disabled={!screenAdmin || !connectedUsers || connectedUsers?.length === 0}
-                                variant="secondary"
-                                title={
-                                    screenAdmin
-                                        ? !connectedUsers || connectedUsers?.length === 0
-                                            ? 'Wait for users to join'
-                                            : ''
-                                        : 'Please select the game on the admin screen'
-                                }
-                            >
-                                Choose Game
-                            </Button>
-
-                            <Button onClick={() => history.push(screenLeaderboardRoute(roomId))}>Leaderboard</Button>
-                            <Button onClick={() => history.push(Routes.screen)}>Back</Button>
-                        </RightButtonContainer>
-                    </RightContainer>
-                </ContentContainer>
-            </Content>
-        </LobbyContainer>
+                    </ContentWrapper>
+                </DialogContent>
+            </Dialog>
+        </>
     );
 };
 
