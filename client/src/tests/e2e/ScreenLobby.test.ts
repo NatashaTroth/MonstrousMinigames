fa1966ea6041bac90d045bbc2d90e95f22cb96fe
Magdalena Maislinger <mmaislinger.mmt-m2020@fh-salzburg.ac.at>
2022-01-27 15:47:07 +0100

    Update gitlab ci

--- client/src/tests/e2e/ScreenLobby.test.ts
+++ client/src/tests/e2e/ScreenLobby.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
 test('Render room code at lobby header', async ({ page }) => {
     await page.goto('/');
@@ -19,3 +19,23 @@ test('Render room code at lobby header', async ({ page }) => {
     const roomCode = page.locator('.roomCode');
     await expect(roomCode).toHaveText(roomId);
 });
+
+// test('Render room code at lobby header', async ({ page }) => {
+//     await page.goto('/');
+
+//     const [response] = await Promise.all([
+//         // Waits for the next response matching some conditions
+//         page.waitForResponse(
+//             response =>
+//                 response.url() === 'https://monstrous-minigames-staging.herokuapp.com/create-room' &&
+//                 response.status() === 200
+//         ),
+//         // Triggers the response
+//         page.locator('button:has-text("Create New Room")').click(),
+//     ]);
+
+//     const { roomId } = await response.json();
+
+//     const roomCode = page.locator('.roomCode');
+//     await expect(roomCode).toHaveText(roomId);
+// });
