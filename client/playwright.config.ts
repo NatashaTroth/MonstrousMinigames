644a86966cc1991bdefedae8aa291ca9dacc0f50
Magdalena Maislinger <mmaislinger.mmt-m2020@fh-salzburg.ac.at>
2022-01-28 09:34:32 +0100

    Update gitlab ci

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -34,7 +34,7 @@ const config: PlaywrightTestConfig = {
         actionTimeout: 0,
 
         /* Base URL to use in actions like `await page.goto('/')`. */
-        //baseURL: 'http://localhost:3000',
+        baseURL: 'http://localhost:3000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
@@ -92,11 +92,11 @@ const config: PlaywrightTestConfig = {
         //   },
         // },
     ],
-    webServer: {
-        command: 'npm run start',
-        port: 3000,
-        timeout: 120 * 1000,
-        reuseExistingServer: !process.env.CI,
-    },
+    // webServer: {
+    //     command: 'npm run start',
+    //     port: 3000,
+    //     timeout: 120 * 1000,
+    //     reuseExistingServer: !process.env.CI,
+    // },
 };
 export default config;
