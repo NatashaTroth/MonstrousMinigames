71a1bdba15043e449e7e18693dbbd5d59601fedb
Robin Fellinger <rfellinger.mmt-m2020@fh-salzburg.ac.at>
2022-01-29 15:27:56 +0100

    ~ longer time until room gets cleaned

--- server/src/enums/globals.ts
+++ server/src/enums/globals.ts
@@ -1,6 +1,6 @@
 export enum Globals {
     MAX_PLAYER_NUMBER = 4,
-    ROOM_TIME_OUT_HOURS = 1,
+    ROOM_TIME_OUT_HOURS = 2,
     GAME_STATE_UPDATE_MS = 33,
     CRON_JOB_CLEANUP = '*/10 * * * *',
     CHARACTER_COUNT = 4,
