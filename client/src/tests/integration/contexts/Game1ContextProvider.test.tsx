3125a651b887f108ad47c2f127f4103885f6b687
Magdalena Maislinger <mmaislinger.mmt-m2020@fh-salzburg.ac.at>
2022-01-31 09:42:41 +0100

    Split tests

--- client/src/tests/integration/contexts/Game1ContextProvider.test.tsx
+++ client/src/tests/integration/contexts/Game1ContextProvider.test.tsx
@@ -1,10 +1,10 @@
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import { configure, shallow } from 'enzyme';
-import React from 'react';
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { configure, shallow } from "enzyme";
+import React from "react";
 
-import Game1ContextProvider from '../../../contexts/game1/Game1ContextProvider';
-import ShakeInstruction from '../../../domain/game1/controller/components/ShakeInstruction';
-import { LocalStorageFake } from '../../integration/storage/LocalFakeStorage';
+import Game1ContextProvider from "../../../contexts/game1/Game1ContextProvider";
+import ShakeInstruction from "../../../domain/game1/controller/components/ShakeInstruction";
+import { LocalStorageFake } from "../storage/LocalFakeStorage";
 
 configure({ adapter: new Adapter() });
 describe('Game1ContextProvider', () => {
