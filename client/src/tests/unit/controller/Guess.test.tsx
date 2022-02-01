cfcadbc2b8f0e75800f4be2ec9d1a1b031b3613f
ash <38651790+mokxshi@users.noreply.github.com>
2022-02-01 12:51:58 +0100

    frontend tests

--- client/src/tests/unit/controller/Guess.test.tsx
+++ client/src/tests/unit/controller/Guess.test.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line simple-import-sort/imports
 import 'jest-styled-components';
-import { cleanup } from '@testing-library/react';
+import { cleanup, queryByText, render } from '@testing-library/react';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { configure, mount } from 'enzyme';
 import { ThemeProvider } from 'styled-components';
@@ -22,5 +22,38 @@ describe('Guess', () => {
         );
 
         expect(container.find('input')).toHaveLength(1);
+    });
+
+    it('renders question', () => {
+        const { container } = render(
+            <ThemeProvider theme={theme}>
+                <Guess />
+            </ThemeProvider>
+        );
+        const questionText = 'How many sheep are on the meadow?';
+        expect(queryByText(container, questionText)).toBeTruthy();
+    });
+
+    it('renders button', () => {
+        const container = mount(
+            <ThemeProvider theme={theme}>
+                <Guess />
+            </ThemeProvider>
+        );
+
+        expect(container.find('button')).toHaveLength(1);
+    });
+
+    it('renders button', () => {
+        const container = mount(
+            <ThemeProvider theme={theme}>
+                <Guess />
+            </ThemeProvider>
+        );
+
+        const btn = container.find('button');
+        btn.simulate('click');
+        const waitingText = 'Waiting for others...';
+        expect(container.findWhere(node => node.text() === waitingText)).toBeTruthy();
     });
 });
