5ac1bf5a49ce7133cefb8c2e94cf1e1278839c3a
Magdalena Maislinger <mmaislinger.mmt-m2020@fh-salzburg.ac.at>
2022-02-03 10:44:43 +0100

    Add e2e test

--- client/src/e2e/ScreenLobby.test.ts
+++ client/src/e2e/ScreenLobby.test.ts
@@ -1,64 +1,64 @@
 import { chromium, devices, expect, test } from "@playwright/test";
 
 test.describe('ScreenLobby', () => {
-    // test('When creating a room on screen, room code should be rendered at lobby header', async ({ baseURL }) => {
-    //     const browser = await chromium.launch({
-    //         args: ['--disable-dev-shm-usage'],
-    //     });
+    test('When creating a room on screen, room code should be rendered at lobby header', async ({ baseURL }) => {
+        const browser = await chromium.launch({
+            args: ['--disable-dev-shm-usage'],
+        });
 
-    //     const context = await browser.newContext({ baseURL });
-    //     const page = await context.newPage();
+        const context = await browser.newContext({ baseURL });
+        const page = await context.newPage();
 
-    //     await page.goto('/');
+        await page.goto('/');
 
-    //     const [response] = await Promise.all([
-    //         // Waits for the next response matching some conditions
-    //         page.waitForResponse(
-    //             response =>
-    //                 response.url() === `${process.env.REACT_APP_BACKEND_URL}create-room` && response.status() === 200
-    //         ),
-    //         // Triggers the response
-    //         page.locator('button:has-text("Create New Room")').click(),
-    //     ]);
+        const [response] = await Promise.all([
+            // Waits for the next response matching some conditions
+            page.waitForResponse(
+                response =>
+                    response.url() === `${process.env.REACT_APP_BACKEND_URL}create-room` && response.status() === 200
+            ),
+            // Triggers the response
+            page.locator('button:has-text("Create New Room")').click(),
+        ]);
 
-    //     const { roomId } = await response.json();
+        const { roomId } = await response.json();
 
-    //     const roomCode = page.locator('.roomCode');
-    //     await expect(roomCode).toHaveText(roomId);
-    // });
+        const roomCode = page.locator('.roomCode');
+        await expect(roomCode).toHaveText(roomId);
+    });
 
-    // test('When joining room on screen, room code should be rendered at lobby header', async ({ baseURL }) => {
-    //     const browser = await chromium.launch({
-    //         args: ['--disable-dev-shm-usage'],
-    //     });
+    test('When joining room on screen, room code should be rendered at lobby header', async ({ baseURL }) => {
+        const browser = await chromium.launch({
+            args: ['--disable-dev-shm-usage'],
+        });
 
-    //     const createRoomBrowser = await browser.newContext({ baseURL });
-    //     const createRoomPage = await createRoomBrowser.newPage();
-    //     await createRoomPage.goto(`/`);
+        const createRoomBrowser = await browser.newContext({ baseURL });
+        const createRoomPage = await createRoomBrowser.newPage();
+        await createRoomPage.goto(`/`);
 
-    //     const [response] = await Promise.all([
-    //         // Waits for the next response matching some conditions
-    //         createRoomPage.waitForResponse(
-    //             response =>
-    //                 response.url() === `${process.env.REACT_APP_BACKEND_URL}create-room` && response.status() === 200
-    //         ),
-    //         // Triggers the response
-    //         createRoomPage.locator('button:has-text("Create New Room")').click(),
-    //     ]);
+        const [response] = await Promise.all([
+            // Waits for the next response matching some conditions
+            createRoomPage.waitForResponse(
+                response =>
+                    response.url() === `${process.env.REACT_APP_BACKEND_URL}create-room` && response.status() === 200
+            ),
+            // Triggers the response
+            createRoomPage.locator('button:has-text("Create New Room")').click(),
+        ]);
 
-    //     const { roomId } = await response.json();
+        const { roomId } = await response.json();
 
-    //     const joinRoomBrowser = await browser.newContext({ baseURL });
-    //     const joinRoomPage = await joinRoomBrowser.newPage();
-    //     await joinRoomPage.goto(`/`);
+        const joinRoomBrowser = await browser.newContext({ baseURL });
+        const joinRoomPage = await joinRoomBrowser.newPage();
+        await joinRoomPage.goto(`/`);
 
-    //     await joinRoomPage.locator('button:has-text("Join Room")').click();
-    //     await joinRoomPage.fill('#joinRoom', roomId);
-    //     await joinRoomPage.locator('button:has-text("Enter")').click();
+        await joinRoomPage.locator('button:has-text("Join Room")').click();
+        await joinRoomPage.fill('#joinRoom', roomId);
+        await joinRoomPage.locator('button:has-text("Enter")').click();
 
-    //     const roomCode = joinRoomPage.locator('.roomCode');
-    //     await expect(roomCode).toHaveText(roomId);
-    // });
+        const roomCode = joinRoomPage.locator('.roomCode');
+        await expect(roomCode).toHaveText(roomId);
+    });
 
     test('When joining with controller, username should be rendered at lobby', async ({ baseURL }) => {
         const browser = await chromium.launch({
@@ -73,8 +73,7 @@ test.describe('ScreenLobby', () => {
             // Waits for the next response matching some conditions
             screenPage.waitForResponse(
                 response =>
-                    response.url() === `https://monstrous-minigames-staging.herokuapp.com/create-room` &&
-                    response.status() === 200
+                    response.url() === `${process.env.REACT_APP_BACKEND_URL}create-room` && response.status() === 200
             ),
             // Triggers the response
             screenPage.locator('button:has-text("Create New Room")').click(),
@@ -107,10 +106,65 @@ test.describe('ScreenLobby', () => {
 
         await controllerPage.locator('button:has-text("Enter")').click();
 
-        await controllerPage.screenshot({ path: 'controller1.png' });
-        await screenPage.screenshot({ path: 'screen1.png' });
-
         const userName = await screenPage.$("text='NameToTest'");
         await expect(userName).toBeTruthy();
+    });
+
+    test('When user is connected and ready, start button should be enabled', async ({ baseURL }) => {
+        const browser = await chromium.launch({
+            args: ['--disable-dev-shm-usage', '--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream'],
+        });
+
+        const screenBrowser = await browser.newContext({ baseURL });
+        const screenPage = await screenBrowser.newPage();
+        await screenPage.goto(`/`);
+
+        const [response] = await Promise.all([
+            // Waits for the next response matching some conditions
+            screenPage.waitForResponse(
+                response =>
+                    response.url() === `${process.env.REACT_APP_BACKEND_URL}create-room` && response.status() === 200
+            ),
+            // Triggers the response
+            screenPage.locator('button:has-text("Create New Room")').click(),
+        ]);
+
+        const { roomId } = await response.json();
+
+        const phone = devices['iPhone 11 Pro'];
+
+        const controllerBrowser = await browser.newContext({
+            baseURL,
+            ...phone,
+        });
+
+        const controllerPage = await controllerBrowser.newPage();
+        await controllerPage.goto(`/`);
+
+        await controllerPage.locator('button:has-text("Skip")').click();
+
+        const elementHandle = await controllerPage.waitForSelector('iframe');
+        const frame = await elementHandle.contentFrame();
+
+        await frame?.waitForSelector('#controllerName');
+        const username = await frame?.$('#controllerName');
+        await username?.type('NameToTest');
+
+        await frame?.waitForSelector('#controllerRoomId');
+        const room = await frame?.$('#controllerRoomId');
+        await room?.type(roomId);
+
+        await controllerPage.locator('button:has-text("Enter")').click();
+        await controllerPage.locator('button:has-text("Choose Character")').click();
+
+        await screenPage.locator('button:has-text("Choose Game")').click();
+        await screenPage.locator(`button:has-text("Start The Great Monster Escape")`).click();
+
+        await controllerPage.locator('#ready').click();
+        const startButton = await screenPage.$('div#startGame');
+
+        await screenPage.screenshot({ path: 'screen1.png' });
+
+        await expect(startButton?.isDisabled()).toBeFalsy();
     });
 });
